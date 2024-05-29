e0204cd8fe8033aba4c6fea17c3c1733fae2d711
SARB <mrsinghbusiness05@gmail.com>
2024-05-29 12:23:54 +0530

    cards created -ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,41 @@ import axios from 'axios';
 
 function App() {
 
-  type AllProducts = {
-     id :number,
-     title : string
-  }
+    type AllProducts = {
+      id :number,
+      title : string,
+      category : string,
+      price : number,
+      images : "",
+      rating : number,
+    }
 
-  const [AllProducts,setAllProducts] = useState<AllProducts[]>([]);
-
-  async function main(){
-    const res = await axios.get('https://dummyjson.com/products');
-    console.log('res is  =',res.data.products);
-    setAllProducts(res.data.products);
-  }
+    const [AllProducts,setAllProducts] = useState<AllProducts[]>([]);
+    async function main(){
+      const res = await axios.get('https://dummyjson.com/products');
+      console.log('res is  =',res.data.products);
+      setAllProducts(res.data.products);
+    }
 
   useEffect(() => {
      main();
   },[])
 
-
   return (
-    <>
-         <h2> 
-          {AllProducts?.map((i) => 
-            <div key = {i?.id}>
-             Id is - {i?.id} || 
-             Title is - {i?.title}
-            </div>
-          )}
-          </h2>
-       <h2> Ecom - app with Toolkit </h2>
+     <>
+      <div style = {{display: "grid" , gridTemplateColumns:'1fr 1fr', gap : '5px' }}>
+            {AllProducts?.map(i => 
+                  <div key = {i?.id} className='card' 
+                  style = {{backgroundColor:'lightgrey',padding:'2%',margin:'5% 1%' , width:'90%'}}>
+                  <div> {i?.id} {i?.title} </div>
+                    <div> <img style = {{width:'55%'}} src = {i?.images[0]}  alt = {i?.title} /> </div> 
+                    <div> Category - {i?.category} </div>
+                    <div> Price -  {i?.price.toFixed()} </div>
+                    <div> Rating - {i?.rating.toFixed()} </div>
+                  </div>
+            )}
+          
+      </div>
     </>
   )
 }
