071c1a52ca8356168ba65ae2838365a7fb9ee844
SARB <mrsinghbusiness05@gmail.com>
2024-05-29 10:59:59 +0530

    data fetchingf -api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
 
+  type AllProducts = {
+     id :number,
+     title : string
+  }
+
+  const [AllProducts,setAllProducts] = useState<AllProducts[]>([]);
+
+  async function main(){
+    const res = await axios.get('https://dummyjson.com/products');
+    console.log('res is  =',res.data.products);
+    setAllProducts(res.data.products);
+  }
+
+  useEffect(() => {
+     main();
+  },[])
+
+
   return (
     <>
-      hleloe 
+         <h2> 
+          {AllProducts?.map((i) => 
+            <div key = {i?.id}>
+             Id is - {i?.id} || 
+             Title is - {i?.title}
+            </div>
+          )}
+          </h2>
+       <h2> Ecom - app with Toolkit </h2>
     </>
   )
 }
