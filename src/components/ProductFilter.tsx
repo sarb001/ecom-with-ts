26fe2860b5cfd2eb2aa5da8ab4f94a5c1b9abefe
SARB <mrsinghbusiness05@gmail.com>
2024-05-30 06:01:56 +0530

    value changes - options

--- src/components/ProductFilter.tsx
+++ src/components/ProductFilter.tsx
@@ -1,8 +1,65 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { useCallback, useState } from "react";
 
 
 const ProductFilter = () => {
+  
+  const { loading , error ,  AllProducts} = useSelector((state : RootState) => state?.mainproduct);
+  
+  console.log('load| erro|allprod=',AllProducts);
+
+  const AllCategory = AllProducts.map(i => i?.category);
+
+  const Uniquecategory = Array.from(new Set(AllCategory));
+  console.log('unique cat =',Uniquecategory);
+
+  const AllPrices = AllProducts.map(i => i?.price.toFixed()); 
+  console.log('allPrices =',AllPrices);        // 1-3000
+
+  const AllRating = AllProducts.map(i => i?.rating.toFixed());
+  console.log('AllRating =',AllRating);        // 3,4,5
+
+  //  Price   - slide ------
+  //  Rating  - checkbox[]
+
+//  const inputchangehandler = useCallback((e:React.ChangeEvent<HTMLInputElement>) => {
+//    setPrice(e.target.value);
+//  },[Price]);
+
+  const [pricevalue,setpricevalue] = useState(0);
+
+  const handleselectionchange = (e:React.ChangeEvent<HTMLSelectElement>) => {
+      const newval = e.target.value;
+      console.log('newval =',newval);
+  };
+
   return (
-    <div>ProductFilter</div>
+    <div>
+         <h3> Filter By Category </h3>
+      <div>
+      {Uniquecategory?.map((i,index) => {
+        return (
+          <div key = {index}>
+             <button style = {{margin:'3% 5%'}}> {i} </button> 
+          </div>
+        )
+      })}
+      </div>
+      <h3> Filter by Price  </h3>
+     
+      <div>
+         <select id = "price" onChange = {(e) => handleselectionchange(e)}>
+           <option  value = "lowest">   Lowest to  Lowest   </option>
+           <option  value = "highest">  Highest by Highest </option>
+         </select>
+      </div>
+
+      <h3> Filter by Rating  </h3>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,7 +6,7 @@ import { AppDispatch, RootState } from "../store/store";
 
 const Products = () => {
 
-  const { loading ,error ,  AllProducts} = useSelector((state : RootState) => state?.mainproduct)
+  const { loading , error ,  AllProducts} = useSelector((state : RootState) => state?.mainproduct)
   console.log('load| erro|allprod=',AllProducts);
 
    const dispatch = useDispatch<AppDispatch>();
@@ -15,31 +15,30 @@ const Products = () => {
       dispatch(AllProductsSlice());
   },[dispatch])
 
+   if(loading) return <h2> Loading...... </h2>
 
   return (
     <>
-     <h3> Product Section  </h3>
-    </>
-    // <>
-    //     <h3> Total Length is -{AllProducts?.length} </h3>
-    //  <div style = {{display: "grid" , gridTemplateColumns:'1fr 1fr', gap : '5px',fontSize:'24px' }}>
-    //         {AllProducts?.map(i => 
-    //               <div key = {i?.id} className='card' 
-    //               style = {{backgroundColor:'lightgrey',padding:'2%',margin:'5% 1%' , width:'90%' , minHeight:'30vh'}}>
-    //               <div> {i?.id} {i?.title} </div>
-    //                 <div style = {{width:'60%' ,height:'30vh'}}> 
-    //                   <img style = {{width:'45%' ,height:'29vh' ,objectFit:'contain'}} src = {i?.images[0]}  alt = {i?.title} />
-    //                 </div> 
-    //                 <div style = {{display:'grid',gridTemplateColumns:'1fr 1fr 1fr',gap:'9px'}}> 
-    //                   <div> {i?.category} Only </div> 
-    //                   <div> Price -  Rs.{i?.price.toFixed()} </div>
-    //                   <div> Rating - {i?.rating.toFixed()} Star </div>
-    //                   </div>
-    //               </div>
-    //         )}
+        <h3> Total Length is -{AllProducts?.length} </h3>
+     <div style = {{display: "grid" , gridTemplateColumns:'1fr 1fr', gap : '5px',fontSize:'24px' }}>
+
+            {AllProducts && AllProducts?.map(i => 
+                  <div key = {i?.id} className='card' 
+                  style = {{backgroundColor:'lightgrey',padding:'2%',margin:'5% 1%' , width:'90%' , minHeight:'30vh'}}>
+                  <div> {i?.id} {i?.title} </div>
+                    <div style = {{width:'60%' ,height:'30vh'}}> 
+                      <img style = {{width:'95%' ,height:'29vh' ,objectFit:'contain'}} src = {i?.images[0]}  alt = {i?.title} />
+                    </div> 
+                    <div style = {{display:'grid',gridTemplateColumns:'1fr 1fr 1fr',gap:'9px'}}> 
+                      <div> {i?.category} Only </div> 
+                      <div> Price -  Rs.{i?.price.toFixed()} </div>
+                      <div> Rating - {i?.rating.toFixed()} Star </div>
+                      </div>
+                  </div>
+            )}
           
-    //   </div>
-    // </>
+      </div>
+    </>
   )
 }
 
